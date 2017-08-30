caf03cb5fe010f2174b36055d8346dfb97f78e9d
Roxanne Baldwin <rox.m.baldwin@gmail.com>
2017-08-30 14:47:58 -0600

    🕺 Users can see a list of messages with the correct styles

--- app/message/message.component.js
+++ app/message/message.component.js
@@ -4,7 +4,25 @@
   .module('inboxApp')
   .component('message', {
     controller: function() {
-      console.log('message working')
+      const vm = this
+          vm.$onInit = function() {
+            var data = angular.fromJson(json)
+            vm.messages = data
+            console.log(vm.messages)
+          }
+
+          vm.markup = function(message) {
+            console.log(message)
+          }
+
+          vm.selectmessage = function(selected) {
+              //    console.log(selected)
+                console.log(vm.anything);
+               }
+
+          function toggleStar() {
+            vm.starred = !vm.starred
+          }
     },
     templateUrl: 'app/message/message.html'
   })
